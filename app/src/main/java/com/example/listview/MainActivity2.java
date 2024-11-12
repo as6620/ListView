f0c7c0ae1a1446c5e78d708c8ab22fcbcaed96dd
as6620 <as6620@bs.amalnet.k12.il>
2024-11-12 19:53:56 +0200

    adapter

--- app/src/main/java/com/example/listview/MainActivity2.java
+++ app/src/main/java/com/example/listview/MainActivity2.java
@@ -2,38 +2,65 @@ package com.example.listview;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity2 extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class MainActivity2 extends AppCompatActivity implements AdapterView.OnItemClickListener{
     ListView listView;
+    String[] StrlV = new String[20];
+    TextView tVX1, tVd, tVn, tVSn;
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        tVX1 = (TextView) findViewById(R.id.tVX1);
+        tVd = (TextView) findViewById(R.id.tVd);
+        tVn = (TextView) findViewById(R.id.tVn);
+        tVSn = (TextView) findViewById(R.id.tVSn);
         listView = (ListView) findViewById(R.id.listView);
+
+        listView.setOnItemClickListener(this);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, StrlV);
+        listView.setAdapter(adp);
 
         Intent gi = getIntent();
         Float x1 = gi.getFloatExtra("firstFlt", 0);
         Float d = gi.getFloatExtra("hefreshOrMahpilFlt", 0);
-        Boolean handasitOrHeshbonit = gi.getBooleanExtra("handasitOrHeshbonit"); //true = Handasit   flase = Heshbonit
+        Boolean handasitOrHeshbonit = gi.getBooleanExtra("handasitOrHeshbonit", false); //true = Handasit   flase = Heshbonit
 
 
-        Intent gi = new Intent();
-        gi.putExtra("x1", x1);
-        gi.putExtra("x2", x2);
-        setResult(RESULT_OK, gi);
-        finish();
+        DecimalFormat df = new DecimalFormat("#.###"); // פורמט לשלוש ספרות אחרי הנקודה
 
-        if(handasitOrHeshbonit) {
-            Float[] lV = new Float[20];
-            for (int i = 0; i < lV.length; i++) {
-                lV[i] = (float) (x1*d^i-1);
-            }
+        for (int i = 0; i<20; i++){
+            if (handasitOrHeshbonit)
+                StrlV[i] = df.format(x1 * i * d); // חשבונית
+            else
+                StrlV[i] = df.format(x1*Math.pow(d,i)); //הנדסית
+
         }
 
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    public void goBack(View view) {
+        finish();
     }
 }
